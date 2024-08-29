90f61c5922e40dadecd3849bbd62bdc060c8e2f1
Adrielly Santos <59398647+adriellyscsantos@users.noreply.github.com>
2024-08-29 16:13:01 -0300

    [Style] Adding style

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
+  <header>
     <h1 v-bind:style="{ textDecoration: state.decoration }">
-      Hello {{ state.name }}
+      Hello {{ state.name }}, seja bem vindo(a)!
     </h1>
-  <input type="text" v-model="state.name" />
+    <input type="text" v-model="state.name" />
+  </header>
   <br />
   <br />
-  <a :href="state.link" target="_blank"> Veja o meu github! </a>
-  <h1>Minha lista de coisas</h1>
-  <button @click="handleShowHideList()">Ver a lista</button>
-  <ul v-if="state.showList">
-    <li
-      v-for="(task, index) in state.tasks"
-      @dblclick="complete(task)"
-      :key="`${task}-${index}`"
-      :class="{ 'line-through': task.isDone }"
-      class="task-item"
-    >
-      {{ task.title }}
-      <button @click="remove(task)">&times;</button>
-    </li>
-  </ul>
-  <p v-else>Lista escondida</p>
-  <h1>Adicione sua lista de coisas!</h1>
-  <input
-    type="text  v-focus"
-    v-model="state.currentTask"
-    @keyup.enter="add()"
-  />
+  <!-- <a :href="state.link" target="_blank"> Veja o meu github! </a> -->
+  <section>
+    <h2>Quais são as suas atividades de hoje?</h2>
+    <p>(Clique duas vezes em cima da atividade para concluí-la)</p>
+    <ul>
+      <li
+        v-for="(task, index) in state.tasks"
+        @dblclick="complete(task)"
+        :key="`${task}-${index}`"
+        :class="{ 'line-through': task.isDone }"
+        class="task-item"
+      >
+        {{ task.title }}
+        <button class="button-remove" @click="remove(task)">&times;</button>
+      </li>
+    </ul>
+    <h3>Digite uma nova atividade e aperte enter!</h3>
+    <input
+      type="text  v-focus"
+      v-model="state.currentTask"
+      @keyup.enter="add()"
+    />
+  </section>
+
+  <article>
+    <button @click="handleShowHideList()">Escolher outra frase</button>
+    <h4 v-if="state.showList">
+      O seu melhor é tudo o que você deve oferecer ao seu dia. Nunca entregue
+      menos que isso.
+    </h4>
+    <h4 v-else>
+      Novas oportunidades em um novo dia é tudo o que você precisa para alcançar
+      seus objetivos.
+    </h4>
+  </article>
+
+  <footer><span> a Vue3 website -- by Adrielly </span></footer>
 </template>
 
 <script>
@@ -45,12 +62,14 @@ export default {
       name: "Adrielly",
       decoration: "underline",
       link: "https://github.com/adriellyscsantos",
-      showList: false,
+      showList: true,
       tasks: [
-        { title: "HTML", isDone: false },
-        { title: "CSS", isDone: false },
-        { title: "JAVASCRIPT", isDone: false },
-        { title: "REACT", isDone: false },
+        { title: "Tomar café da manhã", isDone: false },
+        { title: "Trabalhar", isDone: false },
+        { title: "Almoçar", isDone: false },
+        { title: "Jantar", isDone: false },
+        { title: "Treinar", isDone: false },
+        { title: "Estudar", isDone: false },
       ],
       currentTask: "",
     });
@@ -92,6 +111,85 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+header {
+  background-color: #e91e63;
+  height: 100px;
+}
+
+header h1 {
+  margin: 0;
+  padding: 13px 0px;
+}
+
+h2 {
+  color: #e91e63;
+  margin-bottom: 0;
+}
+
+section {
+  display: block;
+  border: 1px solid #e91e63;
+  padding: 0px 0px 20px 0px;
+}
+section p {
+  font-size: 12px;
+}
+section ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  gap: 10px;
+  text-align: left;
+}
+
+section ul li {
+  list-style: none;
+}
+
+article,
+footer {
+  margin-top: 30px;
+}
+
+button {
+  background-color: #000;
+  border: 1px solid #e91e63;
+  color: #fff;
+  padding: 10px 15px;
+  border-radius: 20px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #e91e63;
+  border: 1px solid #e91e63;
+  transition: 0.8s;
+}
+
+.button-remove {
+  background-color: #e91e63;
+  color: #fff;
+  border-radius: 0;
+  padding: 2px 5px;
+}
+
+input {
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  border: none;
+  padding: 5px 1px;
+}
+
 .line-through {
   text-decoration: line-through;
 }
